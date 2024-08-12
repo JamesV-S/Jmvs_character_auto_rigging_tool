efb087bd4dfb8c145528099535c257008bf330f1
James Vilela-Slater <161130807+JamesV-S@users.noreply.github.com>
2024-08-12 16:17:55 +0100

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,12 @@
+
+import maya.cmds as cmds
+from maya import OpenMayaUI as omui
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import *
+from PySide2.QtUiTools import *
+from shiboken2 import wrapInstance
+
+import os.path
