5d8d4999f367b6964d5200b88a6fe8713ae03350
James Vilela-Slater <161130807+JamesV-S@users.noreply.github.com>
2024-08-12 19:27:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 import importlib
 import sys
 import os
-
+'''
+import maya.cmds as cmds
+script_path = cmds.file(query=True, sceneName=True)
+__file__ = script_path
+'''
 # Add the scripts('main.py') directory to sys.path and MAYA_SCRIPT_PATH
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(script_dir)
-# Get the current scriptpath
 os.environ['MAYA_SCRIPT_PATH'] = script_dir + os.pathsep + os.environ.get('MAYA_SCRIPT_PATH', '')
+# Get the current scriptpath
 # Retrieve Existing MAYA_SCRIPT_PATH: 'os.environ.get('MAYA_SCRIPT_PATH', '')'
 # Concatenate Paths: 'os.pathsep' is the separator for the operating system
 # Update enviroment Variable: updated path assigned back to 'os.environ['MAYA_SCRIPT_PATH']'
@@ -21,4 +25,4 @@ importlib.reload(ui)
 def run_ui():
     ui.main()
     print("main is run")
-# run_ui()
+run_ui()
