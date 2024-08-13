bc7c218f3fd417508c06e495ee8b6472db67ff4c
James Vilela-Slater <161130807+JamesV-S@users.noreply.github.com>
2024-08-13 17:56:54 +0100

    Update ui.py
    Updated the ui with Blueprints menu drop down button

--- ui.py
+++ ui.py
@@ -8,6 +8,9 @@ from PySide2.QtWidgets import QWidget
 from PySide2.QtWidgets import *
 from PySide2.QtUiTools import *
 from shiboken2 import wrapInstance
+from PySide2 import QtUiTools, QtWidgets, QtCore
+from functools import partial # if you want to include args with UI method calls
+
 
 import os.path
 import importlib
@@ -29,21 +32,48 @@ class QtSampler(QWidget):
         
         # self.update_d
 
-        # Tab 1
-        '''
-        self.ui.scale_spnB.valueChanged.connect(self.scale_of_ctrl)
-        self.ui.axs_ddbx.currentIndexChanged.connect(self.axs_dir_ddbox)
-        self.ui.zero_out_checkBx.stateChanged.connect(self.clean_ctrls)
-        self.ui.num_ctrl_line.textChanged.connect(self.num_ctrl_input)
-        self.ui.new_ctrl_ddp.currentIndexChanged.connect(self.cr_nw_ctrl)
-        self.ui.type_ctrl_ddbox.currentIndexChanged.connect(self.ctrl_type_func)
-        self.ui.sys_type_line.textChanged.connect(self.sys_type)
-        self.ui.colour_line.textChanged.connect(self.ctl_colour)
-        self.ui.apply_btn.clicked.connect(self.apply_func)
-        '''
-        # Tab 2
+        ''' A method to get the icon Directory!
+        tool_file = __file__
+        tool_file = tool_file.replace( '\\', '/')
 
-        # Tab 3
+        if '_tool.py' in tool_file:
+            product = os.path.basename( tool_file ).split( '_tool.py' )[0]
+        else:
+            product = os.path.basename( tool_file ).split( '.py' )[0]
+
+        product_fullName = 'UltimateAdjustableCarRig'
+        tool_folder = os.path.dirname( tool_file ) + '/'
+        rig_folder = tool_folder + 'rigs/'
+        tool_fileName = os.path.basename( tool_file ).split( '.py' )[0]
+
+        abrev = 'UACR'
+        iconDir = tool_folder + 'icons/'
+        '''
+        #self.ui.hand_module_btn.clicked.connect(self.temp_hand_func)
+        # Tab 1 - RIG
+        
+        # Access the blueprints_toolbtn
+        parent_widget = self.ui.findChild(QtWidgets.QWidget, "tab_rig")
+        if parent_widget:
+            print("found parent widget")
+            self.blueprints_toolbtn = parent_widget.findChild(QtWidgets.QToolButton, "blueprints_menu_toolbtn")
+        else:
+            print("couldn't find parent widget")
+        # set the popup mode for blueprints_toolbtn
+        if self.blueprints_toolbtn:
+            print(f"Found blueprints_menu_toolbtn in the ui")
+            self.blueprints_toolbtn.setPopupMode(QtWidgets.QToolButton.MenuButtonPopup)
+            self.create_popup_menu()
+        else:
+            print(f"could not find blueprints_menu_toolbtn in the ui")
+        
+        self.ui.blueprints_menu_toolbtn.clicked.connect(self.blueprints_menu_func)
+        
+        # Tab 2 - SKINNING
+
+        # Tab 3 - CURVE HELPER
+
+        # Tab 4 - OUTPUT OPTIONS(export)
 
     # functions, connected to above commands   
     def initUI(self):
@@ -52,7 +82,7 @@ class QtSampler(QWidget):
         # Constructing the UI File Path
         UI_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
                                "interface", f"Jmvs_character_auto_rigger_{UI_VERSION}.ui")
-        print(f"UI Jmvs_CAR file path: {UI_FILE}")
+        print(f"UI Jmvs_Character auto rigger file path: {UI_FILE}")
         # Instead of writing the path manually: Gets absolute path of ui file, 
         # as os.path.join() constructs a path by combining the directory with 
         # the relative path:  ui.py directory + "interface\\Jmvs_character_auto_rigger_001.ui".
@@ -69,7 +99,39 @@ class QtSampler(QWidget):
         file.close()
     #--------------------------------------------------------------------------     
     
-       
+    def create_popup_menu(self):
+        # Create a QMenu
+        menu = QMenu(self)
+
+        # Create actions for the buttons
+        biped_action = QAction("Biped - Basic", self)
+        quad_action = QAction("Quad - Basic", self)
+
+        # Connect the actions to their respective fucntions.
+        biped_action.triggered.connect(self.load_biped_basic_blueprint)
+        quad_action.triggered.connect(self.load_quad_basic_blueprint)
+
+        # Add actions to the menu
+        menu.addAction(biped_action)
+        menu.addAction(quad_action)
+
+        # Set the menu to the blueprints_toolbtn
+        self.blueprints_toolbtn.setMenu(menu)
+    
+    def blueprints_menu_func(self):
+        # Define the functionality for blueprints_toolbtn here
+        print("blueprints menu button clicked")
+    
+    def load_biped_basic_blueprint(self):
+        # Define the functionality for Biped basic button here 
+        print("Biped basic button clicked")
+    
+    def load_quad_basic_blueprint(self):
+        # Define the functionality for Quad basic button here
+        print("Quad basic button clicked")
+
+    def temp_hand_func(self):
+        print("button hand!!!!!!!!!")
 def main():
     ui = QtSampler()
     ui.show()
