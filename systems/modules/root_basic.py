132577abd00f5e6f766f1b1a8a46265f1a5bb8fb
James Vilela-Slater <161130807+JamesV-S@users.noreply.github.com>
2024-09-11 14:53:48 +0100

    Update root_basic.py

--- systems/modules/root_basic.py
+++ systems/modules/root_basic.py
@@ -1,7 +1,8 @@
 system = ["root", "COG"]
-system_pos = {"root": [0,0,0], "root": [0,150,0]}
-system_rot = {"root": [0,0,0], "root": [0,0,0]}
+system_pos = {"root": [0,0,0], "COG": [0,150,0]}
+system_rot = {"root": [0,0,0], "COG": [0,0,0]}
 side = "None"
+orientation = "None"
 space_swapping = []
 guide_scale = 1
 available_rig_types = ["FK"]
