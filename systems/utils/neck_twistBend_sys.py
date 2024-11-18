815613081e331c48e45b8594ca340baf8cf6f36e
JamesV-S <hamzvilelas@gmail.com>
2024-11-18 20:11:02 +0000

    Neck_sys WIP

--- systems/utils/neck_twistBend_sys.py
+++ systems/utils/neck_twistBend_sys.py
@@ -101,6 +101,8 @@ class neck_sys():
         numLS = create_list_from_integer(self.neck_amnt)
         twist_neg_name = "jnt_TwistNeg"
         ctrl_neck_list = []
+        self.jnt_twistNeg_list = []
+        self.jnt_att_list = []
         for i in range(self.neck_amnt): # 2 wth 3 from ui          
             jnt_att_neck = f"{self.pref_list[0]}_{self.first_guide[6:-2]}_{numLS[i]}"
             print(f"NName of att joint: {jnt_att_neck}")
@@ -108,7 +110,7 @@ class neck_sys():
             print(f"jnt_att_match_to: {self.jntAtt_match[i]}")
             cmds.matchTransform(jnt_att_neck, self.jntAtt_match[i], pos=1, rot=1, scl=0)
             cmds.makeIdentity(jnt_att_neck, a=1, t=0, r=1, s=0)
-
+            self.jnt_att_list.append(jnt_att_neck)
             #-------------
             self.ctrl_att_neck = f"{self.pref_list[1]}_{self.first_guide[6:-2]}_{numLS[i]}"
             print(f"NECK_ATT: guide for CTRL > {self.ctrl_att_neck}")
@@ -120,14 +122,13 @@ class neck_sys():
             ctrl_neck_list.append(self.ctrl_att_neck)
 
             #-------------
-            jnt_TwistNeg_name = f"{twist_neg_name}_{self.first_guide[6:-2]}_{numLS[i]}"
-            cmds.joint(n=jnt_TwistNeg_name)
-            cmds.matchTransform(jnt_TwistNeg_name, self.jntAtt_match[i])
-            cmds.makeIdentity(jnt_TwistNeg_name, a=1, t=0, r=1, s=0)
+            self.jnt_TwistNeg = f"{twist_neg_name}_{self.first_guide[6:-2]}_{numLS[i]}"
+            cmds.joint(n=self.jnt_TwistNeg)
+            cmds.matchTransform(self.jnt_TwistNeg, self.jntAtt_match[i]) 
+            cmds.makeIdentity(self.jnt_TwistNeg, a=1, t=0, r=1, s=0)
+            self.jnt_twistNeg_list.append(self.jnt_TwistNeg)
             
-            cmds.parentConstraint( jnt_att_neck, self.ctrl_att_neck, w=1, mo=0 )
            
-
     def end_guide_att(self):
         bend_neg_name =  "jnt_BendNeg"
 
@@ -137,6 +138,7 @@ class neck_sys():
         cmds.matchTransform(jnt_att_end, self.head_guide, pos=1, rot=1, scl=0)
         cmds.makeIdentity(jnt_att_end, a=1, t=0, r=1, s=0)
         # Don't need to opm clean ctrls cus they will be zeroed out by the jnt's above them in Hi!
+        self.jnt_att_list.append(jnt_att_end)
 
         #-------------
         jnt_bendNeg_end = f"{bend_neg_name}_{self.head_guide[6:]}"
@@ -154,11 +156,12 @@ class neck_sys():
         cmds.matchTransform(self.ctrl_att_head, self.head_guide, pos=1, rot=1, scl=0)
         cmds.parent(self.ctrl_att_head, jnt_bendNeg_end)
 
+
     def add_attr(self):
         # create the custom attrib's on head control
         utils.add_locked_attrib(ctrl=self.ctrl_att_head, en=["NECK_SYS"])
         utils.add_float_attrib(ctrl=self.ctrl_att_head, flt=["Neck_Twist_Mult"], 
-                               val=[0,1], limited=True)
+                               val=[0,0.95], limited=True)
         utils.add_float_attrib(ctrl=self.ctrl_att_head, flt=["Neck_Bend_Mult"], 
                                val=[0,1], limited=True)
         cmds.setAttr( f"{self.ctrl_att_head}.Neck_Twist_Mult", 0.5 )
@@ -166,7 +169,10 @@ class neck_sys():
 
 
     def add_nodes(self):
-        pass
+        # self.jnt_twistNeg_list = []
+        
+        cmds.setAttr(f"{self.ctrl_att_head}.rotateX", 90)
+
         N_root_AttMd = f"MD_att_RT_{self.first_guide[6:-2]}"
         print(f"neck_root > {N_root_AttMd}") # MD_neck_0_neck_root
         utils.cr_node_if_not_exists(util_type=1, node_type="multiplyDivide", 
@@ -182,39 +188,108 @@ class neck_sys():
         utils.cr_node_if_not_exists(util_type=1, node_type="unitConversion", 
                                 node_name=minTws, set_attrs={"conversionFactor": -1})
         
-        # # to UnitConv & Minus & to Mult
+        # to UnitConv & Minus & to Mult
         utils.connect_attr(f"{self.ctrl_att_head}.Neck_Twist_Mult", 
-                           f"{minTws}.input")
+                            f"{minTws}.input")
         utils.connect_attr(f"{minTws}.output", 
-                           f"{N_root_NegMd}.input1{self.twist_axis}")
+                            f"{N_root_NegMd}.input1{self.twist_axis}")
         
         utils.connect_attr(f"{self.ctrl_att_head}.Neck_Twist_Mult", 
-                           f"{N_root_AttMd}.input1{self.twist_axis}")
+                            f"{N_root_AttMd}.input1{self.twist_axis}")
         utils.connect_attr(f"{self.ctrl_att_head}.rotate{self.twist_axis}", 
-                           f"{N_root_NegMd}.input2{self.twist_axis}")
+                            f"{N_root_NegMd}.input2{self.twist_axis}")
 
-        # cmds.connectAttr( (self.ctrlLs[-1] + ".Neck_Twist_Mult"), (minTws + ".input"), f=1 )
-        # cmds.connectAttr( (minTws + ".output"), (f"{N_root_NegMd}.input1{self.twistAxis}"), f=1 )
-        # cmds.connectAttr( (self.ctrlLs[-1] + ".Neck_Twist_Mult"), (f"{N_root_AttMd}.input1{self.twistAxis}"), f=1 )
-        # cmds.connectAttr( (self.ctrlLs[-1] + ".rotate" + self.twistAxis), (f"{N_root_NegMd}.input2{self.twistAxis}"), f=1 )
-        
-        '''
+
         # Connect up to the att jnt!
+        print(f"att_Jnts_Ls = {self.jnt_att_list}")
+        print(f"att_Jnts_Ls[-2] = {self.jnt_att_list[-2]}")
         utils.connect_attr(f"{N_root_AttMd}.output{self.twist_axis}", 
-                           f"{minTws}.rotate")
-        cmds.connectAttr( (f"{N_root_AttMd}.output{self.twistAxis}"), (f"{self.attJntsLs[-2]}.rotate{self.twistAxis}"), f=1 )
-        
-        # Connect to the Neg twist for same jnt
-        cmds.connectAttr( (f"{N_root_NegMd}.output{self.twistAxis}"), (f"{self.TwistNeg[-2]}.rotate{self.twistAxis}"), f=1 )
-        '''
+                           f"{self.jnt_att_list[-2]}.rotate{self.twist_axis}")
 
-        '''
-        N_root_AttMd = cmds.shadingNode("multiplyDivide", au=1, n="neck_root" + "_att_MD")
-        cmds.connectAttr( (f"{self.ctrlLs[-1]}.rotate{self.twistAxis}"), (f"{N_root_AttMd}.input2{self.twistAxis}"), f=1 )
-        N_root_NegMd = cmds.shadingNode("multiplyDivide", au=1, n="neck_root" + "_neg_MD")
-        minTws = cmds.shadingNode("unitConversion", au=1, n="neck_twist" + "_minus_UC")
-        cmds.setAttr( minTws + ".conversionFactor", -1 )
-        '''
+
+        print(f"twist_neg_list = {self.jnt_twistNeg_list}")
+        print(f"twist_neg_list[-1] = {self.jnt_twistNeg_list[-1]}")
+        utils.connect_attr(f"{N_root_NegMd}.output{self.twist_axis}", 
+                            f"{self.jnt_twistNeg_list[-1]}.rotate{self.twist_axis}")
+        # ['jnt_TwistNeg_neck_1', 'jnt_TwistNeg_neck_2', 'jnt_BendNeg_head']
+        
+
+        #--------------------------------------------------------------------------------------
+        # TWIST SYS - page 1-2
+        #--------------------------------------------------------------------------------------
+        # Twist moving down the chain: get value from parent att_Md node, 
+        # plug that into 2 new att/Neg Md nodes for the current joint working on!
+        # create fucntion that does this, so for the amount of neck joints, call this function that many times to 
+        # dissiminate the twisting of the neck!
+        # (the next chain always comes from the previous att_md node)
+        # These joints need to get half of the value from it's parent jnt!
+
+        divisible_value = 2 # Constant value
+        
+        # [neck_01, neck_02, neck_03, neck_04, head] - neck just before head is the neck_root! 
+        # Everything before 'neck_04' is a list of joints
+
+        just_neck_list = self.jnt_att_list[:-1] # remove head joint
+        jnt_attNeck_children = just_neck_list[:-1]
+        # ['jnt_att_0_neck_1', 'jnt_att_0_neck_2']
+        jnt_attNeck_children.reverse() # reverse the order cus we're working form head down to bottom of neck joints.
+        print(f"jnt_attNeck_children = {jnt_attNeck_children}")
+
+        twisNeg_children = self.jnt_twistNeg_list[:-1]
+        twisNeg_children.reverse()
+        print(f"twisNeg_children = {twisNeg_children}")
+
+        print(f"AYY == MD_att_{self.first_guide[6:-2]}")
+        # AYY == MD_att_0_neck
+        
+        # -------------------------
+        # build MD for eaxh neck att & do connections
+        neck_order = create_list_from_integer(len(jnt_attNeck_children))
+        neck_order.reverse()
+
+        order_list = [self.divisibleList[i] for i in range(len(jnt_attNeck_children))]
+        for x in range(len(jnt_attNeck_children)):
+            # Set the att_Md input2X to positive 'Divisible_value' & neg_Md to negative 'Divisible_value'
+            MD_1_att = f"MD_att_{self.first_guide[6:-2]}_{neck_order[x]}"
+            utils.cr_node_if_not_exists(util_type=1, node_type="multiplyDivide", 
+                                        node_name=MD_1_att, 
+                                        set_attrs={f"input2{self.twist_axis}":order_list[x], "operation":2})
+            
+            MD_1_neg = f"MD_neg_{self.first_guide[6:-2]}_{neck_order[x]}"
+            utils.cr_node_if_not_exists(util_type=1, node_type="multiplyDivide", 
+                                        node_name=MD_1_neg, 
+                                        set_attrs={f"input2{self.twist_axis}":-order_list[x], "operation":2})
+            
+            utils.connect_attr(f"{N_root_AttMd}.output{self.twist_axis}", f"{MD_1_att}.input1{self.twist_axis}")
+            utils.connect_attr(f"{N_root_AttMd}.output{self.twist_axis}", f"{MD_1_neg}.input1{self.twist_axis}")
+            
+            # Connect to the att & Neg joint!
+            utils.connect_attr(f"{MD_1_att}.output{self.twist_axis}", f"{jnt_attNeck_children[x]}.rotate{self.twist_axis}")
+            utils.connect_attr(f"{MD_1_neg}.output{self.twist_axis}", f"{twisNeg_children[x]}.rotate{self.twist_axis}")
+        
+        
+        #--------------------------------------------------------------------------------------
+        # Bend SYS - page 4-5
+        #--------------------------------------------------------------------------------------
+        bend_ratio = f"MD_{self.first_guide[6:-2]}_bendRatio"
+        utils.cr_node_if_not_exists(util_type=1, node_type="multiplyDivide", 
+                                        node_name=bend_ratio, 
+                                        set_attrs={"operation":2,})
+        
+        # begin with the 'N_root_AttMd'+ connect bending axes to it!
+        for x in range(2):
+            utils.connect_attr(f"{self.ctrl_att_head}.rotate{self.bend_axis[x]}", f"{N_root_AttMd}.input2{self.bend_axis[x]}")
+            utils.connect_attr(f"{self.ctrl_att_head}.Neck_Bend_Mult", f"{N_root_AttMd}.input1{self.bend_axis[x]}")
+            cmds.setAttr(f"{bend_ratio}.input2{[self.bend_axis[x]]}", divisible_value)
+            utils.connect_attr(f"{N_root_AttMd}.output{self.bend_axis[x]}", f"{bend_ratio}.input1{self.bend_axis[x]}")
+
+        # Connect bend_ratio to all neck joints 
+        # just_neck_list
+        
+                
+
+        
+
 
     def get_ctrls(self):
         return self.fk_ctrls
